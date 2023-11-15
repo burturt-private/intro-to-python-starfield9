0b3ef19408ee4ab420fc58ba9bf82f57268fdb74
starfield9 <76552680+starfield9@users.noreply.github.com>
2023-11-15 01:27:04 -0800

    Update grade.sh

--- autograde/grade.sh
+++ autograde/grade.sh
@@ -3,6 +3,6 @@ echo $(docker --version)
 
 docker compose -f docker-compose.yaml up --abort-on-container-exit --build
 
-exit $(docker inspect test-python-autograde-autograde-1 --format='{{.State.ExitCode}}')
+# exit $(docker inspect test-python-autograde-autograde-1 --format='{{.State.ExitCode}}')
 
-# exit $(docker compose ps -aq | xargs docker inspect -f '{{ .Name }} {{ .State.ExitCode }}' | grep 'autograde-script' | cut -d ' ' -f2)
+exit $(docker compose ps -aq | xargs docker inspect -f '{{ .Name }} {{ .State.ExitCode }}' | grep 'autograde-script' | cut -d ' ' -f2)
